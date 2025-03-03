805fec21b2842048c02bfcbbca66eb37d9d59610
davidMarostica <davidmarosticasilvasilva25@gmail.com>
2025-03-03 13:15:24 -0300

    Adiciona classes Aluno e Principal para gerenciamento de alunos

---
+++ src/Principal.java
@@ -0,0 +1,9 @@
+public class Principal {
+    public static void main(String[] args) {
+        // Instanciando a classe Aluno
+        Aluno aluno1 = new Aluno("João Silva", "Rua das Flores, 123", "11987654321", "joao.silva@example.com", "20230001");
+
+        // Exibindo informações do aluno
+        aluno1.exibirInformacoes();
+    }
+}
